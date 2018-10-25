10c6f8a2cf5e5f456ba5811a8b1b192b72fb7949
HikaruLim <linxrh@live.com>
2018-10-25 16:43:07 +0800

    user can edit profile

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 # 这个是设计数据库表的，所谓的“model”
 
@@ -13,6 +14,9 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(128))
 
     posts = db.relationship('Post',backref='author',lazy='dynamic')
+
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime,default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)   
@@ -30,6 +34,11 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash,password)
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
+        
 
 class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
